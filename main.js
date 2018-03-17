2519f45f16136231c75582041e85e82520bfc361
mohammed shahabuddein akheeb <37287235+sakheeb@users.noreply.github.com>
2018-03-17 16:17:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,12 @@ const routes = require('./routes');
 
 const server = new Hapi.Server();
 
+/*
 server.connection({
     port: 80,
-	host: "192.168.0.10"
+    host: "192.168.0.10"
 });
+*/
 server.ext('onPreResponse', corsHeaders);
 
 
