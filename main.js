42c0aeaf2b9316cb18dd57623ae1ee5d092ef381
sakheeb <shahabuddeinakheeb786@gmail.com>
2018-03-17 16:23:29 +0530

    hello world

--- main.js
+++ main.js
@@ -1,30 +1,12 @@
-'use strict';
+var http = require("http");
 
-var Hapi = require('hapi');
-var corsHeaders = require('hapi-cors-headers')
-const routes = require('./routes');
+http.createServer(function (request, response) {
 
-
-const server = new Hapi.Server();
-
-server.connection({
-    port: 80,
-	host: "192.168.0.10"
-});
-server.ext('onPreResponse', corsHeaders);
-
-
-server.register([], function(err) {
-
-    if (err) {
-        console.error('Failed loading plugins');
-        process.exit(1);
-    }
-
-    server.route(routes);
-    
-    server.start(function () {
-        console.log('Server running at:', server.info.uri);
-    });
-});
-
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8080);
